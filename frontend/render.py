5042c36a0c17bc567fee5c9a14bd4ea207186f27
Artem Kurta <artemdog386@gmail.com>
2026-05-22 01:22:18 +0300

    full merge 2 branches

--- frontend/render.py
+++ frontend/render.py
@@ -4,8 +4,8 @@ import pygame
 
 from backend.core.game_map import GameMap, Vec2, WALL, DOT, ENERGIZER, GHOST_HOUSE
 from backend.entities.pacman import PacMan
-from backend.entities.ghost import Ghost, GhostMode, GhostPersonality
-from backend.core.game_engine import GameState
+from backend.entities.ghost import Ghost, GhostMode
+from backend.core import game_engine
 from backend.utils.config import GameConfig
 
 
